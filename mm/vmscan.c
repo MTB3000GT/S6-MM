dbdef4f3cc88fc309b7a0c062a90ef8083c10696
Minchan Kim <minchan@kernel.org>
2013-05-09 16:21:27 +0900

    mm: Enhance per process reclaim to consider shared pages
    Some pages could be shared by several processes. (ex, libc)
    In case of that, it's too bad to reclaim them from the beginnig.
    
    This patch causes VM to keep them on memory until last task
    try to reclaim them so shared pages will be reclaimed only if
    all of task has gone swapping out.
    
    This feature doesn't handle non-linear mapping on ramfs because
    it's very time-consuming and doesn't make sure of reclaiming and
    not common.
    
    Change-Id: I7e5f34f2e947f5db6d405867fe2ad34863ca40f7
    Signed-off-by: Sangseok Lee <sangseok.lee@lge.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 9 May 2013 16:21:27
    [vinmenon@codeaurora.org: trivial merge conflict fixes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    
    Conflicts:
    	mm/vmscan.c

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -97,6 +97,13 @@ struct scan_control {
 	 * are scanned.
 	 */
 	nodemask_t	*nodemask;
+
+	/*
+	 * Reclaim pages from a vma. If the page is shared by other tasks
+	 * it is zapped from a vma without reclaim so it ends up remaining
+	 * on memory until last task zap it.
+	 */
+	struct vm_area_struct *target_vma;
 };
 
 #define lru_to_page(_head) (list_entry((_head)->prev, struct page, lru))
@@ -838,7 +845,8 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 		 * processes. Try to unmap it here.
 		 */
 		if (page_mapped(page) && mapping) {
-			switch (try_to_unmap(page, ttu_flags)) {
+			switch (try_to_unmap(page,
+					ttu_flags, sc->target_vma)) {
 			case SWAP_FAIL:
 				goto activate_locked;
 			case SWAP_AGAIN:
@@ -1039,6 +1047,7 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 }
 
 #ifdef CONFIG_PROCESS_RECLAIM
+<<<<<<< HEAD
 static unsigned long shrink_page(struct page *page,
 					struct zone *zone,
 					struct scan_control *sc,
@@ -1061,7 +1070,8 @@ static unsigned long shrink_page(struct page *page,
 	return reclaimed;
 }
 
-unsigned long reclaim_pages_from_list(struct list_head *page_list)
+unsigned long reclaim_pages_from_list(struct list_head *page_list,
+					struct vm_area_struct *vma)
 {
 	struct scan_control sc = {
 		.gfp_mask = GFP_KERNEL,
@@ -1069,6 +1079,7 @@ unsigned long reclaim_pages_from_list(struct list_head *page_list)
 		.may_writepage = 1,
 		.may_unmap = 1,
 		.may_swap = 1,
+		.target_vma = vma,
 	};
 
 	LIST_HEAD(ret_pages);
